981b76df0b73c24f4d1d8b58672a05e750ead21e
Ishwar Chaudhary <ishwarura@gmail.com>
2023-07-09 22:47:39 +0545

    REG PAGE AND PACKAGE UPDATE

--- lib/presentation/register/register_page.dart
+++ lib/presentation/register/register_page.dart
@@ -1,5 +1,8 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
@@ -9,6 +12,25 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+
+  File? _profileImage;
+
+  final _signupFormKey = GlobalKey<FormState>();
+
+  //Profile image picker
+
+  Future _pickProfileImage() async {
+    try {
+      final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+      if(image == null) return;
+
+      final imageTemporary = File(image.path);
+      setState(() => _profileImage = imageTemporary);
+    } on PlatformException catch (e) {
+      debugPrint('Failed to pick image error: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
