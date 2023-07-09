8d96f93ebce7ae03a7991fb8c383c6a4d9b73c0d
Ishwar Chaudhary <ishwarura@gmail.com>
2023-07-10 00:01:23 +0545

    Update login_page.dart

--- lib/presentation/login/login_page.dart
+++ lib/presentation/login/login_page.dart
@@ -6,6 +6,7 @@ import '../../utils/widgets/input_textfield.dart';
 import '../components/custom_button.dart';
 import '../components/page_header.dart';
 import '../components/page_heading.dart';
+
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
@@ -21,118 +22,147 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-          backgroundColor: const Color(0xffEEF1F3),
-          body: Column(
-            children: [
-              const PageHeader(),
-              Expanded(
-                child: Container(
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.vertical(top: Radius.circular(20),),
-                  ),
-                  child: SingleChildScrollView(
-                    child: Form(
-                      key: _loginFormKey,
-                      child: Column(
-                        children: [
-                          const PageHeading(title: 'Log-in',),
-                          CustomInputField(
-                            labelText: 'Email',
-                            hintText: 'Your email id',
-                            validator: (textValue) {
-                              if(textValue == null || textValue.isEmpty) {
-                                return 'Email is required!';
-                              }
-                              if(!EmailValidator.validate(textValue)) {
-                                return 'Please enter a valid email';
-                              }
-                              return null;
+      backgroundColor: const Color(0xffEEF1F3),
+      body: Column(
+        children: [
+          const PageHeader(),
+          Expanded(
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(20),
+                ),
+              ),
+              child: SingleChildScrollView(
+                child: Form(
+                  key: _loginFormKey,
+                  child: Column(
+                    children: [
+                      const PageHeading(
+                        title: 'Log-in',
+                      ),
+                      CustomInputField(
+                          labelText: 'Email',
+                          hintText: 'Your email id',
+                          validator: (textValue) {
+                            if (textValue == null || textValue.isEmpty) {
+                              return 'Email is required!';
                             }
+                            if (!EmailValidator.validate(textValue)) {
+                              return 'Please enter a valid email';
+                            }
+                            return null;
+                          }),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      CustomInputField(
+                        labelText: 'Password',
+                        hintText: 'Your password',
+                        obscureText: true,
+                        suffixIcon: true,
+                        validator: (textValue) {
+                          if (textValue == null || textValue.isEmpty) {
+                            return 'Password is required!';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      Container(
+                        width: size.width * 0.80,
+                        alignment: Alignment.centerRight,
+                        child: GestureDetector(
+                          // onTap: () => {
+                          //   Navigator.push(context, MaterialPageRoute(builder: (context) => const ForgetPasswordPage()))
+                          // },
+                          child: const Text(
+                            'Forget password?',
+                            style: TextStyle(
+                              color: Color(0xff939393),
+                              fontSize: 13,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
-                          const SizedBox(height: 16,),
-                          CustomInputField(
-                            labelText: 'Password',
-                            hintText: 'Your password',
-                            obscureText: true,
-                            suffixIcon: true,
-                            validator: (textValue) {
-                              if(textValue == null || textValue.isEmpty) {
-                                return 'Password is required!';
-                              }
-                              return null;
-                            },
-                          ),
-                          const SizedBox(height: 16,),
-                          Container(
-                            width: size.width * 0.80,
-                            alignment: Alignment.centerRight,
-                            child: GestureDetector(
-                              // onTap: () => {
-                              //   Navigator.push(context, MaterialPageRoute(builder: (context) => const ForgetPasswordPage()))
-                              // },
-                              child: const Text(
-                                'Forget password?',
-                                style: TextStyle(
-                                  color: Color(0xff939393),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      CustomFormButton(
+                        innerText: 'Login',
+                        onPressed: _handleLoginUser,
+                      ),
+                      const SizedBox(
+                        height: 18,
+                      ),
+                      SizedBox(
+                        width: size.width * 0.8,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text(
+                              'Don\'t have an account ? ',
+                              style: TextStyle(
                                   fontSize: 13,
-                                  fontWeight: FontWeight.bold,
-                                ),
+                                  color: Color(0xff939393),
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            GestureDetector(
+                              onTap: () => {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => RegisterPage()))
+                              },
+                              child: const Text(
+                                'Sign-up',
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    color: Color(0xff748288),
+                                    fontWeight: FontWeight.bold),
                               ),
                             ),
-                          ),
-                          const SizedBox(height: 20,),
-                          CustomFormButton(innerText: 'Login', onPressed: _handleLoginUser,),
-                          const SizedBox(height: 18,),
-                          SizedBox(
-                            width: size.width * 0.8,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                const Text('Don\'t have an account ? ', style: TextStyle(fontSize: 13, color: Color(0xff939393), fontWeight: FontWeight.bold),),
-                                GestureDetector(
-                                  onTap: () => {
-                                    Navigator.push(context, MaterialPageRoute(builder: (context) => RegisterPage()))
-                                  },
-                                  child: const Text('Sign-up', style: TextStyle(fontSize: 15, color: Color(0xff748288), fontWeight: FontWeight.bold),),
-                                ),
-                              ],
-                            ),
-                          ),
-                          const SizedBox(height: 20,),
-                        ],
+                          ],
+                        ),
                       ),
-                    ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                    ],
                   ),
                 ),
               ),
-              Center(
-                          child: Column(
-                            children: [
-                              Text(
-                                "© ${DateTime.now().year} Uranus HRM",
-                                style: const TextStyle(
-                                    color: AppColors.primaryColor, fontSize: 12),
-                              ),
-                              Text(
-                                "Version $versionName",
-                                style: const TextStyle(
-                                    color: AppColors.primaryColor, fontSize: 10),
-                              ),
-                            ],
-                          ),
-                        ),
-            ],
+            ),
           ),
-        );
+          Center(
+            child: Column(
+              children: [
+                Text(
+                  "© ${DateTime.now().year} Smart Traffic Nepal",
+                  style: const TextStyle(
+                      color: AppColors.primaryColor, fontSize: 12),
+                ),
+                Text(
+                  "Version $versionName",
+                  style: const TextStyle(
+                      color: AppColors.primaryColor, fontSize: 10),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
   void _handleLoginUser() {
     // login user
     if (_loginFormKey.currentState!.validate()) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Submitting data..')),
-      );
+      Navigator.pushNamed(context, '/home');
     }
   }
 }
