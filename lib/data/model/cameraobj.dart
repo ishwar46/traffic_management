2e222dacfe60986e0d9849abfa1e933c2bc88958
Ishwar Chaudhary <ishwarura@gmail.com>
2023-07-26 21:26:05 +0545

    Update cameraobj.dart

--- lib/data/model/cameraobj.dart
+++ lib/data/model/cameraobj.dart
@@ -5,7 +5,6 @@ class CameraData {
 
   CameraData(this.cameraId, this.datetime, this.objectId);
 
-  // Add a factory constructor to create an instance of CameraData from a Map
   factory CameraData.fromMap(Map<dynamic, dynamic> map) {
     return CameraData(
       map['camera_id'],
