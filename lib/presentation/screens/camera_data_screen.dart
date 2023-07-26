0a0f226b7cb105d58e106c5c6f40cf366ded4eb4
Ishwar Chaudhary <ishwarura@gmail.com>
2023-07-26 21:24:17 +0545

    Update camera_data_screen.dart

--- lib/presentation/screens/camera_data_screen.dart
+++ lib/presentation/screens/camera_data_screen.dart
@@ -1,1 +1,48 @@
+// import 'package:firebase_database/firebase_database.dart';
+// import 'package:flutter/material.dart';
 
+// class RealtimeDataScreen extends StatefulWidget {
+//   @override
+//   _RealtimeDataScreenState createState() => _RealtimeDataScreenState();
+// }
+
+// class _RealtimeDataScreenState extends State<RealtimeDataScreen> {
+//   final DatabaseReference _databaseReference = FirebaseDatabase.instance.ref('CCTV').child('');
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Vehicle Data'),
+//       ),
+//       body: StreamBuilder<Map<dynamic, dynamic>>(
+//         stream: _databaseReference.onValue,
+//         builder: (context, snapshot) {
+//           if (snapshot.hasData && snapshot.data != null) {
+//             Map<dynamic, dynamic> data = snapshot.data!.snapshot.value;
+//             List<dynamic> dataList = data.values.toList();
+//             return ListView.builder(
+//               itemCount: dataList.length,
+//               itemBuilder: (context, index) {
+//                 String objectId = dataList[index]['object_id'];
+//                 String cameraId = dataList[index]['camera_id'];
+//                 String dateTime = dataList[index]['datetime'];
+
+//                 Map<dynamic, dynamic> dataMap = data as Map<dynamic, dynamic>;
+
+//                 return ListTile(
+//                   title: Text('Object ID: $objectId'),
+//                   subtitle: Text('Camera ID: $cameraId\nDate: $dateTime'),
+//                 );
+//               },
+//             );
+//           } else {
+//             return const Center(
+//               child: CircularProgressIndicator(),
+//             );
+//           }
+//         },
+//       ),
+//     );
+//   }
+// }
