8f8a74df376c71110118c4fd2baf45fc2bf6ca47
Roz Maharjan <rox.maharjan@gmail.com>
2023-08-11 21:35:04 +0545

    Create new_custom.dart

---
+++ lib/presentation/components/new_custom.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:trafficnepal/utils/app_colors.dart';
+
+class CustomFormButton extends StatelessWidget {
+  final String innerText;
+  final void Function()? onPressed;
+
+  const CustomFormButton({
+    Key? key,
+    required this.innerText,
+    required this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      width: 360,
+      height: 50,
+      decoration: BoxDecoration(
+        color: AppColors.primaryColor,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: TextButton(
+        onPressed: onPressed,
+        child: Text(
+          innerText,
+          style: const TextStyle(color: Colors.white, fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
